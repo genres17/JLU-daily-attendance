6f1020fb8100abcb92ee5c1229af9eab36e5005e
genres17 <3461372627@qq.com>
2020-09-26 10:00:11 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # 这里是存储一些事先要准备数据的地方
 # 用户名和密码
-username = 'yinbh20'
-password = 'ybh261426160628'
+username = ''
+password = ''
 
 # 打卡需要填写内容，因为每天都需要重填
-major = '电子信息'  # 专业名称
-grade = '2020'  # 年级
-school_district = '中心校区' # 所属校区
-department = '文苑6公寓' # 公寓名称
-room_number = '415' # 寝室号
+major = ''  # 专业名称
+grade = ''  # 年级
+school_district = '' # 所属校区
+department = '' # 公寓名称
+room_number = '' # 寝室号
 
